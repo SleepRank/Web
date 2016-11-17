2af1a873203daef15471e72a48b5460e03b2f764
wei170 <guocheng@guochengwei.com>
2016-11-17 01:35:53 -0500

    continue testing

--- server/app.js
+++ server/app.js
@@ -105,11 +105,12 @@ mongodb.MongoClient.connect(URI, function (err, database) {
 	db = database;
 	console.log("Database connection ready");
 
-	// Initialize the app.
-	var server = app.listen(process.env.PORT || 8080, function () {
-		var port = server.address().port;
-		console.log("App now running on port", port);
-	});
+});
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+	var port = server.address().port;
+	console.log("App now running on port", port);
 });
 
 
